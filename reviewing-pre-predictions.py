02eb14bd2c7844c6489e80843c2565bee0f2f3af
Jairus <241715b@student.hci.edu.sg>
2026-07-13 16:38:02 +0800

    solving for model accuracy score

--- reviewing-pre-predictions.py
+++ reviewing-pre-predictions.py
@@ -1,12 +1,14 @@
 from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
 
-dir_path = Path(r"C:\Users\Jairus\Desktop\ulti-player-detection\training-labels\night-pretrain\labels")
+dir_path = Path(r"C:\Users\parent\Documents\GitHub\ultimate-player-tracking\night-pretrain\labels")
 check_id = '0' #for multi object if necessary
 
 
 lows = []
+highs = []
 average = []
-
 
 
 # result = [s.split(' ') for s in strings]
@@ -33,10 +35,38 @@ for item in dir_path.iterdir():
     for confidence in confidence_list:
         curr_sum += float(confidence)
     
+
+    
     average.append(curr_sum / len(confidence_list))
     lows.append(float(min(confidence_list)))
+    highs.append(float(max(confidence_list)))
+    
 
-# print(f"averages = {average}\n lows = {lows}")
-print(f"averages of data = {sum(average) / len(average)}, {sum(lows) / len(lows)}")
+print(f"""
+        averages confidence = {sum(average) / len(average)}
+        worse average = {min(average)}, frame number: {average.index(min(average))}
+        lows: {min(lows)}, {sum(lows) / len(lows)}
+        highs: {max(highs)}, {sum(highs) / len(highs)}
+        
+""")
         
 
+#create graphs
+def plotter(lst, name):
+    x = []
+    y = []
+    
+    for val in lst:
+        x.append(lst.index(val))
+        y.append(val)
+
+    plt.plot(x, y, 'o', color='red')
+    plt.title(name)
+    plt.ylim(0.00, 1.00)
+    plt.show()
+
+
+#average confidence
+plotter(average, 'average')
+plotter(highs, 'highs')
+plotter(lows, 'lows')
