30110d0e2bfdababf5225efc307b46c6eac11132
Jairus <241715b@student.hci.edu.sg>
2026-07-16 23:21:39 +0800

    tidied up hsv mapper, resolves #3
    introduced new parameter for custom mapping

--- Libraries/feature_extraction.py
+++ Libraries/feature_extraction.py
@@ -44,8 +44,8 @@ def normalize_coordinates(image_path:str, label:str, convert: bool):
             return[int(cy - (h/2)), int(cy + (h/2)), int(cx - (w/2)), int(cx + (w/2))]
 
 
-def generate_hsv_map(img_source:str, save:bool, show:bool, part):
-      #part 0 = hue, part 1 = saturation, part 2 = value
+def generate_hsv_map(img_source:str, save:bool, show:bool, part, test:any):
+      #part 0 = hue, part 1 = saturation, part 2 = value, part 3 = test
 
       
       img = cv2.imread(f"{img_source}")
@@ -54,54 +54,54 @@ def generate_hsv_map(img_source:str, save:bool, show:bool, part):
 
       image = []
 
-      if part == 0:
-            for row in H:
+      for count_out, row in enumerate(H):
+            row_pixels = []
+            for count_in, value in enumerate(row):
+                  current_H = int(value)
+                  current_S = int(S[count_out][count_in])
+                  current_V = int(V[count_out][count_in])
 
-                  row_pixels = []
-                  for value in row:
+                  if part == 0:
+                        row_pixels.append([current_H, 255, 255])
+                  elif part == 1:
+                        row_pixels.append([179, current_S, 255])
+                  elif part == 2:
+                        row_pixels.append([179, 255, current_V])
+                  elif part == 3:
+                        builder = []
+                        if test[0] == 1:
+                              builder.append(current_H)
+                        else:
+                              builder.append(179)
+                        if test[1] == 1:
+                              builder.append(current_S)
+                        else:
+                              builder.append(255)
+                        if test[2] == 1:
+                              builder.append(current_V)
+                        else:
+                              builder.append(255)
 
-                        row_pixels.append([int(value), 255, 255])
+                        row_pixels.append(builder)
 
-                  image.append(row_pixels)
+                  else:
+                        #fail case, invalid part num
+                        return "Invalid part number"
+                  
+            image.append(row_pixels)
 
-      elif part == 1:
-            for row in S:
-                  row_pixels = []
-                  for value in row:
 
-                        row_pixels.append([179, int(value), 255])
 
-                  image.append(row_pixels)
-
-      elif part == 2:
-            for row in V:
-                  row_pixels = []
-                  for value in row:
-
-                        row_pixels.append([179, 255, int(value)])
-
-                  image.append(row_pixels)
-      elif part == 3:
-            for out_count, row in enumerate(H):
-                  row_pixels = []
-                  for in_count, value in enumerate(row):
-
-                        row_pixels.append([120, int(S[out_count][in_count]), int(V[out_count][in_count])])
-     
-                  image.append(row_pixels)
-
-      else:
-            return "Invalid part number"
-
+      print("completed image generation")
       hsv_image = np.array(image, dtype=np.uint8)
       bgr = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
 
       if save:
-            cv2.imwrite(f"{img_source[:-4]}-{part}.png", bgr)
+            cv2.imwrite(f"{img_source[:-4]}-{part}br.png", bgr)
 
       if show:
             cv2.imshow("image", bgr)
             cv2.waitKey(0)
 
 
-generate_hsv_map(r"demo/HSV Map demo/full-frame.png", True, False, 3)
+generate_hsv_map(r"demo/HSV Map demo/full-frame.png", True, False, 3, [1,1,0])
